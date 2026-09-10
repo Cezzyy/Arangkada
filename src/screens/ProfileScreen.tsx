588219a6d5c3df26b602094caf4b5d478fa772a2
John Cez Casupanan <casupananjohncez@gmail.com>
2026-09-11 03:18:51 +0800

    refactor: simplify media type picker

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -44,7 +44,7 @@ export default function ProfileScreen() {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: 'images',
       allowsEditing: true,
       aspect: [4, 3],
       quality: 0.8,
